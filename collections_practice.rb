63d913a093eb920c812d7ababb695e3a8de22945
Diana Utan <didiutzi@gmail.com>
2020-11-30 00:39:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,12 +41,12 @@ def sum_array(arr)
   arr.reduce(0, :+)
 end
 
-def add_s(array)
-  array.collect do |word|
-    if array[1] == word
-      word
-    else
-      word + "s"
-    end
-  end
-end
+# def add_s(array)
+#   array.collect do |word|
+#     if array[1] == word
+#       word
+#     else
+#       word + "s"
+#     end
+#   end
+# end
